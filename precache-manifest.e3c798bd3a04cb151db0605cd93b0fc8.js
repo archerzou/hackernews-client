8834c43d6ff7f351ab70c19a95bda2551d16bc9c
kevinwit <kevin.zou@honeywell.com>
2019-12-13 10:33:54 +0800

    Updates

---
+++ precache-manifest.e3c798bd3a04cb151db0605cd93b0fc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae7cd3c16d1d4c72a68de8e93cb356f",
+    "url": "/hackernews-client/index.html"
+  },
+  {
+    "revision": "5be7fcfd9a9089b78f2b",
+    "url": "/hackernews-client/static/css/main.8528c43f.chunk.css"
+  },
+  {
+    "revision": "b27b84e495cc01c9a68b",
+    "url": "/hackernews-client/static/js/2.eac878d1.chunk.js"
+  },
+  {
+    "revision": "5be7fcfd9a9089b78f2b",
+    "url": "/hackernews-client/static/js/main.7af79403.chunk.js"
+  },
+  {
+    "revision": "e8138d85219934b7e088",
+    "url": "/hackernews-client/static/js/runtime~main.36d84e0f.js"
+  }
+]);
